fb2dc1c6c6dc595da937cd8b919c6cb48c8ab320
tomoya187 <187tomo@gmail.com>
2023-04-28 15:07:16 -0400

    Updates

---
+++ precache-manifest.0f5100fac461040bd2cf75e66070eef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "00c977c1264eef907012",
+    "url": "/react-portfolio/static/css/main.0770e42e.chunk.css"
+  },
+  {
+    "revision": "00c977c1264eef907012",
+    "url": "/react-portfolio/static/js/main.00c977c1.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-portfolio/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/react-portfolio/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "43b4fc953a87d38a66ff",
+    "url": "/react-portfolio/static/js/runtime~main.43b4fc95.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5caafb8bc90eff533b7286e163afcd9c",
+    "url": "/react-portfolio/index.html"
+  }
+];
